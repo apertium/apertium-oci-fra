19c2071c4b19655bf17c751f6e625b8d6a53a2d4
Hèctor Alòs i Font <hectoralos@gmail.com>
2018-07-07 15:45:40 +0300

    correccion

--- retraduir_fra_oci.sh
+++
@@ -1,2 +0,0 @@
-make && cat ../apertium-fra/corpus/wikipedia.5000.txt | apertium -d . fra-oci > traduc_nova_fra_oci.txt
-diff traduc_act_fra_oci.txt traduc_nova_fra_oci.txt | more
--- retraduir_oci_fra.sh
+++
@@ -1,2 +0,0 @@
-make && cat ../apertium-oci/corpus/jornalet.5000.txt | apertium -d . oci-fra > traduc_nova_oci_fra.txt
-diff traduc_act_oci_fra.txt traduc_nova_oci_fra.txt | more
