2c8d68fcad4df0286e60983c8031ef5936cbe01c
hectoralos <hectoralos@gmail.com>
2021-10-31 12:04:32 +0300

    +gascon

--- dev/testvoc/inconsistency.sh
+++ dev/testvoc/inconsistency.sh
@@ -16,14 +16,46 @@ lt-expand $2 | grep -v REGEX | grep -v '<prn><enc>' | sed 's/:>:/%/g' | grep -v 
         lt-proc -d ../../oci-fra.autogen.bin > $TMPDIR/tmp_testvoc3.txt
 paste -d _ $TMPDIR/tmp_testvoc1.txt $TMPDIR/tmp_testvoc2.txt $TMPDIR/tmp_testvoc3.txt | sed 's/\^.<sent>\$//g' | sed 's/_/   --------->  /g' | grep -v '\^@'
 
+elif [[ $1 = "oci_gascon-fra" ]]; then
+
+lt-expand $2 | grep -v REGEX | grep -v '<prn><enc>' | sed 's/:>:/%/g' | grep -v ':<:' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |\
+        apertium-pretransfer|\
+        lt-proc -b ../../oci@gascon-fra.autobil.bin |\
+        lrx-proc -m ../../oci-fra.autolex.bin |\
+        apertium-transfer -b ../../apertium-oci-fra.oci-fra.t1x  ../../oci-fra.t1x.bin |\
+        apertium-interchunk ../../apertium-oci-fra.oci-fra.t2ax  ../../oci-fra.t2ax.bin |\
+        apertium-interchunk ../../apertium-oci-fra.oci-fra.t2bx  ../../oci-fra.t2bx.bin |\
+        apertium-interchunk ../../apertium-oci-fra.oci-fra.t2cx  ../../oci-fra.t2cx.bin |\
+        apertium-postchunk ../../apertium-oci-fra.oci-fra.t3x  ../../oci-fra.t3x.bin |\
+        apertium-transfer -n ../../apertium-oci-fra.oci-fra.t4x  ../../oci-fra.t4x.bin |\
+	tee $TMPDIR/tmp_testvoc2.txt |\
+        lt-proc -d ../../oci-fra.autogen.bin > $TMPDIR/tmp_testvoc3.txt
+paste -d _ $TMPDIR/tmp_testvoc1.txt $TMPDIR/tmp_testvoc2.txt $TMPDIR/tmp_testvoc3.txt | sed 's/\^.<sent>\$//g' | sed 's/_/   --------->  /g' | grep -v '\^@'
+
 elif [[ $1 = "fra-oci" ]]; then
 
 lt-expand $2 | grep -v REGEX | grep -v '<prn><enc>' | sed 's/:>:/%/g' | grep -v ':<:' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |\
         apertium-pretransfer|\
         lt-proc -b ../../fra-oci.autobil.bin |\
         lrx-proc -m ../../fra-oci.autolex.bin |\
-        apertium-transfer -b ../../apertium-oci-fra.fra-oci.t1x  ../../fra-oci.t1x.bin  |\
+        apertium-transfer -b ../../.deps/fra-oci.t1x  ../../fra-oci.t1x.bin  |\
         apertium-interchunk ../../apertium-oci-fra.fra-oci.t2x  ../../fra-oci.t2x.bin  |\
+        apertium-postchunk ../../apertium-oci-fra.fra-oci.t3x  ../../fra-oci.t3x.bin  |\
+        apertium-transfer -n ../../apertium-oci-fra.fra-oci.t4x  ../../fra-oci.t4x.bin  |\
+	tee $TMPDIR/tmp_testvoc2.txt |\
+        lt-proc -d ../../fra-oci.autogen.bin > $TMPDIR/tmp_testvoc3.txt
+paste -d _ $TMPDIR/tmp_testvoc1.txt $TMPDIR/tmp_testvoc2.txt $TMPDIR/tmp_testvoc3.txt | sed 's/\^.<sent>\$//g' | sed 's/_/   --------->  /g' | grep -v '\^@'
+
+elif [[ $1 = "fra-oci_gascon" ]]; then
+
+lt-expand $2 | grep -v REGEX | grep -v '<prn><enc>' | sed 's/:>:/%/g' | grep -v ':<:' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |\
+        apertium-pretransfer|\
+        lt-proc -b ../../fra-oci@gascon.autobil.bin |\
+        lrx-proc -m ../../fra-oci.autolex.bin |\
+        apertium-transfer -b ../../.deps/fra-oci@gascon.t1x  ../../fra-oci@gascon.t1x.bin  |\
+        apertium-interchunk ../../apertium-oci-fra.fra-oci.t2x  ../../fra-oci.t2x.bin  |\
+        apertium-interchunk ../../apertium-oci-fra.fra-oci.t2x_supersn  ../../fra-oci@gascon.t2x_supersn.bin  |\
+        apertium-interchunk ../../apertium-oci-fra.fra-oci.t2x_enon  ../../fra-oci@gascon.t2x_enon.bin  |\
         apertium-postchunk ../../apertium-oci-fra.fra-oci.t3x  ../../fra-oci.t3x.bin  |\
         apertium-transfer -n ../../apertium-oci-fra.fra-oci.t4x  ../../fra-oci.t4x.bin  |\
 	tee $TMPDIR/tmp_testvoc2.txt |\
--- dev/testvoc/testvoc.sh
+++ dev/testvoc/testvoc.sh
@@ -1,5 +1,11 @@
-echo "==French->Occitan==========================";
-bash inconsistency.sh fra-oci ../../../apertium-fra/.deps/fra.dix > /tmp/fra-oci.testvoc; bash inconsistency-summary.sh /tmp/fra-oci.testvoc fra-oci 
+echo "==Languedocien French->Occitan==========================";
+bash inconsistency.sh fra-oci ../../../apertium-fra/.deps/fra.dix > /tmp/fra-oci.testvoc; bash inconsistency-summary.sh /tmp/fra-oci.testvoc fra-oci
 echo ""
-echo "==Occitan->French===========================";
+echo "==French->Gascon Occitan==========================";
+bash inconsistency.sh fra-oci_gascon ../../../apertium-fra/.deps/fra.dix > /tmp/fra-oci_gascon.testvoc; bash inconsistency-summary.sh /tmp/fra-oci_gascon.testvoc fra-oci
+echo ""
+echo "==Languedocien Occitan->French===========================";
 bash inconsistency.sh oci-fra ../../../apertium-oci/.deps/oci.dix > /tmp/oci-fra.testvoc; bash inconsistency-summary.sh /tmp/oci-fra.testvoc oci-fra
+echo ""
+echo "==Gascon Occitan->French===========================";
+bash inconsistency.sh oci_gascon-fra ../../../apertium-oci/.deps/oci@gascon.dix > /tmp/oci_gascon-fra.testvoc; bash inconsistency-summary.sh /tmp/oci_gascon-fra.testvoc oci-fra
