e9052706fa19aa352b73b8e2205cbb2da3ebe0bf
hectoralos <hectoralos@gmail.com>
2021-10-31 16:43:46 +0300

    correccions

--- dev/testvoc/testvoc.sh
+++ dev/testvoc/testvoc.sh
@@ -2,10 +2,10 @@ echo "==Languedocien French->Occitan==========================";
 bash inconsistency.sh fra-oci ../../../apertium-fra/.deps/fra.dix > /tmp/fra-oci.testvoc; bash inconsistency-summary.sh /tmp/fra-oci.testvoc fra-oci
 echo ""
 echo "==French->Gascon Occitan==========================";
-bash inconsistency.sh fra-oci_gascon ../../../apertium-fra/.deps/fra.dix > /tmp/fra-oci_gascon.testvoc; bash inconsistency-summary.sh /tmp/fra-oci_gascon.testvoc fra-oci
+bash inconsistency.sh fra-oci_gascon ../../../apertium-fra/.deps/fra.dix > /tmp/fra-oci_gascon.testvoc; bash inconsistency-summary.sh /tmp/fra-oci_gascon.testvoc fra-oci_gascon
 echo ""
 echo "==Languedocien Occitan->French===========================";
 bash inconsistency.sh oci-fra ../../../apertium-oci/.deps/oci.dix > /tmp/oci-fra.testvoc; bash inconsistency-summary.sh /tmp/oci-fra.testvoc oci-fra
 echo ""
 echo "==Gascon Occitan->French===========================";
-bash inconsistency.sh oci_gascon-fra ../../../apertium-oci/.deps/oci@gascon.dix > /tmp/oci_gascon-fra.testvoc; bash inconsistency-summary.sh /tmp/oci_gascon-fra.testvoc oci-fra
+bash inconsistency.sh oci_gascon-fra ../../../apertium-oci/.deps/oci@gascon.dix > /tmp/oci_gascon-fra.testvoc; bash inconsistency-summary.sh /tmp/oci_gascon-fra.testvoc oci_gascon-fra
